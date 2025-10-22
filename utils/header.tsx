984d604890b8dac00b6c20d33377032f2a437bb0
t1ger <tigertv112@gmail.com>
2025-10-22 15:11:38 +0100

    Added navbar link functionalities and stylings

--- utils/header.tsx
+++ utils/header.tsx
@@ -6,20 +6,28 @@ export default function Header() {
     
     <div className="navbar-container">
 
-      <div className="nav-btn">
-        <a className="font-navbar" href="#">HOME</a>
+      <div className="nav-left-group">
+        <div className="nav-btn">
+          <a className="font-navbar" href="#home">HOME</a>
+        </div>
+
+        <div className="nav-btn">
+          <a className="font-navbar" href="#skills">ACTIVITY</a>
+        </div>
+
+        <div className="nav-btn">
+          <a className="font-navbar" target="_blank" href="https://github.com/t1ger112">GITHUB</a>
+        </div>
+
+        <div className="nav-btn">
+          <a className="font-navbar" href="#about">ABOUT</a>
+        </div>
       </div>
 
-      <div className="nav-btn">
-        <a className="font-navbar" href="#">PAGE1</a>
-      </div>
-
-      <div className="nav-btn">
-        <a className="font-navbar" href="#">PAGE2</a>
-      </div>
-
-      <div className="nav-btn">
-        <a className="font-navbar" href="#">ABOUT</a>
+      <div className="nav-right-group">
+        <div className="nav-btn">
+          <a className="font-navbar" target="_blank" href="https://linkedin.com/in/freddiemichaelrobinson">CONTACT</a>
+        </div>
       </div>
 
     </div>
