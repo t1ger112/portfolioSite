1764152d98120d4165a4e4ed755814c448fe8a83
t1ger112 <freddiemichaelrobinson@gmail.com>
2026-05-20 21:06:08 +0100

    map viewer testing

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,5 +1,7 @@
 
-import GalleryViewer from "@/utils/galleryViewer";
+'use client';
+
+// import GalleryViewer from "@/utils/galleryViewer";
 import ThemeSlider from "@/utils/themeSlider";
 import Image from "next/image";
 import Link from "next/link";
@@ -24,11 +26,19 @@ import {
   MarqueeItem,
 } from "@/components/kibo-ui/marquee";
 
-import { Map, MapControls } from "@/components/ui/map";
+import { Map, MapControls, MapViewport } from "@/components/ui/map";
 import { Card } from "@/components/ui/card";
+import { useState } from "react";
 
 
-export default async function Gallery() {
+export default  function Gallery() {
+
+   const [viewport, setViewport] = useState<MapViewport>({
+    center: [10, 50],
+    zoom: 3,
+    bearing: 0,
+    pitch: 0,
+  });
 
 
   return (
@@ -66,10 +76,32 @@ export default async function Gallery() {
 
               <div className="flex size-full items-center justify-center bg-background">
 
-                <Card className="h-100 w-full p-0 overflow-hidden">
-                  <Map center={[-74.006, 40.7128]} zoom={11}>
-                    <MapControls />
+                <Card className="h-100 w-full p-0 overflow-hidden relative">
+                  <Map viewport={viewport} onViewportChange={setViewport} >
+                    <MapControls showFullscreen showCompass showZoom showLocate />
                   </Map>
+                  <div className="bg-background/80 absolute top-2 left-2 z-10 flex flex-wrap gap-x-3 gap-y-1 rounded border px-2 py-1.5 font-mono text-xs backdrop-blur">
+                    <span>
+                      <span className="text-muted-foreground">lng:</span>{" "}
+                      {viewport.center[0].toFixed(3)}
+                    </span>
+                    <span>
+                      <span className="text-muted-foreground">lat:</span>{" "}
+                      {viewport.center[1].toFixed(3)}
+                    </span>
+                    <span>
+                      <span className="text-muted-foreground">zoom:</span>{" "}
+                      {viewport.zoom.toFixed(1)}
+                    </span>
+                    <span>
+                      <span className="text-muted-foreground">bearing:</span>{" "}
+                      {viewport.bearing.toFixed(1)}°
+                    </span>
+                    <span>
+                      <span className="text-muted-foreground">pitch:</span>{" "}
+                      {viewport.pitch.toFixed(1)}°
+                    </span>
+                  </div>
                 </Card>
                 
               </div>
